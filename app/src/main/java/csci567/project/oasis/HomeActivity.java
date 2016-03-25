65d2fa6014ba8ee8b0fa06bd010d29f4a8733789
Lucas Nager <lnager@mail.csuchico.edu>
2016-03-25 14:25:00 -0700

    Home Activity
    Setting up a simple design for our Home/Main screen.

--- app/src/main/java/csci567/project/oasis/HomeActivity.java
+++ app/src/main/java/csci567/project/oasis/HomeActivity.java
@@ -2,12 +2,33 @@ package csci567.project.oasis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
+    @Bind(R.id.scan_history) ListView qr_scan_history;
+
+
+    @OnClick(R.id.scan_button) public void scan() {
+        Log.d(TAG, "Scan Button Clicked");
+    }
+    @OnClick(R.id.locate_button) public void locate() {
+        Log.d(TAG, "Locate Button Clicked");
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
     }
 }
