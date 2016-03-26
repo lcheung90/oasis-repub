24d7e61db345b48d94ea7e5a025d153fff9b11dc
Anna Paula Maule <annamaule@usp.br>
2016-03-26 15:18:07 -0700

    made the button 'locate' clickable and start a new intent

--- app/src/main/java/csci567/project/oasis/HomeActivity.java
+++ app/src/main/java/csci567/project/oasis/HomeActivity.java
@@ -1,7 +1,10 @@
 package csci567.project.oasis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button buttonLocate = (Button) findViewById(R.id.btn_locate);
+        assert buttonLocate != null;
+        buttonLocate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                locate();
+            }
+        });
     }
+
+    public void locate(){
+        Intent intent = new Intent(this, MapsActivity.class);
+    }
+
 }
