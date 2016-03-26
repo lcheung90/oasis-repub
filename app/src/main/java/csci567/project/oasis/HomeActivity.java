258cd9213af2bd1b233549c23af6380d20ea82de
Lucas Nager <lnager@mail.csuchico.edu>
2016-03-25 23:35:28 -0700

    Home Activity (adjustments)
    Played with color palettes, and implemented a simple RecyclerView to
    eventually display your history of QR code scanning.

--- app/src/main/java/csci567/project/oasis/HomeActivity.java
+++ app/src/main/java/csci567/project/oasis/HomeActivity.java
@@ -2,9 +2,11 @@ package csci567.project.oasis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.ListView;
 import android.widget.Toast;
+
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -13,14 +15,27 @@ import butterknife.OnClick;
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivity";
-    @Bind(R.id.scan_history) ListView qr_scan_history;
+
+
+    String[] code = {"650VC2H","B71FP2Z"};
+    String[] loc = {"Yolo Hall", "O'Connell"};
+    String[] day = {"03/26/16 11:15 A.M.", "03/26/16 1:05 P.M."};
+
+
+
+
+
+
+
 
 
     @OnClick(R.id.scan_button) public void scan() {
         Log.d(TAG, "Scan Button Clicked");
+        //Toast.makeText(Context)
     }
     @OnClick(R.id.locate_button) public void locate() {
         Log.d(TAG, "Locate Button Clicked");
+        //Toast.makeText(Context)
     }
 
 
@@ -30,5 +45,16 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
+
+        RecyclerView qr_scan_history = (RecyclerView) findViewById(R.id.scan_history);
+        RecyclerView.Adapter adapter = new RecyclerAdapter(code, loc, day);
+        RecyclerView.LayoutManager layout_manager = new LinearLayoutManager(this);
+
+
+        qr_scan_history.setLayoutManager(layout_manager);
+        qr_scan_history.setHasFixedSize(true);
+        qr_scan_history.setAdapter(adapter);
+
+
     }
 }
---
+++ app/src/main/java/csci567/project/oasis/RecyclerAdapter.java
@@ -0,0 +1,74 @@
+package csci567.project.oasis;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by 6661 on 3/25/2016.
+ */
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> {
+
+
+    String[] code, loc, day;
+
+    public RecyclerAdapter(String[] code, String[] loc, String[] day){
+        this.code = code;
+        this.loc = loc;
+        this.day = day;
+    }
+
+
+
+
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout,parent,false);
+        RecyclerViewHolder recyclerViewHolder = new RecyclerViewHolder(view);
+        return recyclerViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        holder.xx_Code.setText(code[position]);
+        holder.xx_Loc.setText(loc[position]);
+        holder.xx_Day.setText(day[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return loc.length;
+    }
+
+
+
+
+
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        TextView xx_Code, xx_Loc, xx_Day;
+
+        public RecyclerViewHolder(View view){
+            super(view);
+            xx_Code = (TextView)view.findViewById(R.id.xx_code);
+            xx_Loc = (TextView)view.findViewById(R.id.xx_loc);
+            xx_Day = (TextView)view.findViewById(R.id.xx_day);
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
