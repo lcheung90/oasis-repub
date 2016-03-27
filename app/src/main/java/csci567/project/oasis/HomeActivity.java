6a776aa24684e8c54a4f5711c35d837d9bcefd56
Luis Cheung <lcheung90@gmail.com>
2016-03-27 02:30:15 -0700

    [Feature] Register button and textview for qr code
    Add click listener to launch QRScanActivity

--- app/src/main/java/csci567/project/oasis/HomeActivity.java
+++ app/src/main/java/csci567/project/oasis/HomeActivity.java
@@ -1,13 +1,33 @@
 package csci567.project.oasis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView barcodeInfo;
+    private Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        scanButton = (Button) findViewById(R.id.btn_scan);
+        barcodeInfo = (TextView) findViewById(R.id.tv_debug_qrresult);
+        scanButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent scanner = new Intent(HomeActivity.this, QRScanActivity.class);
+                        startActivity(scanner);
+                    }
+                }
+        );
     }
 }
