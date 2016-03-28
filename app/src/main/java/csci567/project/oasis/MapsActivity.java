fc4d02ba70c4d7b2f96b2a68a3bd41431cb3e4dd
Anna Paula Maule <annamaule@usp.br>
2016-03-27 19:30:11 -0700

    Demo of my functionalities almost done

--- app/src/main/java/csci567/project/oasis/MapsActivity.java
+++ app/src/main/java/csci567/project/oasis/MapsActivity.java
@@ -1,46 +1,108 @@
 package csci567.project.oasis;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Bind;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+
+    private GoogleMap googleMap;
 
     private GoogleMap mMap;
+    private LatLngBounds CHICO = new LatLngBounds(
+            new LatLng(39.725193, -121.848341), new LatLng(39.734038, -121.843255));
+
+    private static final LatLng LIBRARY = new LatLng(39.728123, -121.846275);
+    public final static String EXTRA_MARKER = "csci567/project/oasis.MARKER";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+               .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+//    private void addGoogleMap() {
+//        // check if we have got the googleMap already
+//        if (googleMap == null) {
+//            googleMap = ((SupportMapFragment) getSupportFragmentManager()
+//                    .findFragmentById(R.id.map)).getMap();
+//            googleMap.setOnMarkerClickListener(this);
+//            googleMap.setOnMarkerDragListener(this);
+//        }
+//
+//    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap = googleMap;
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng csuChico = new LatLng(39.73, -121.84);
+        mMap.setOnMarkerClickListener( this);
+        mMap.addMarker(new MarkerOptions().position(csuChico).title("Marker in Chico State University"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(CHICO, 0));
+        markers(mMap);
+
+
+       // mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(30), 2000, null);
+
     }
+
+    public void markers(GoogleMap googleMap){
+        Marker library = googleMap.addMarker(new MarkerOptions()
+                .position(LIBRARY)
+                .title("Library")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+    }
+
+
+    public boolean onMarkerClick(Marker marker) {
+
+        Intent intent = new Intent(this, BuildingInfo.class);
+        String title = marker.getTitle();
+        intent.putExtra(EXTRA_MARKER, title);
+        //i.putExtra("str1", db.getCulturaName(arg0.getTitle()).getDescription());
+        startActivity(intent);
+        return false;
+    }
+
 }
