cf51bf72571795cb03140328c8c5ef8d7de9fb73
Luis Cheung <lcheung90@gmail.com>
2016-04-25 22:55:18 -0700

    [Feature] Add Bluemix authentication client code
    - Modify onActivityResult method to distinguish between scan result and
    authentication result
    - Define request codes

--- app/src/main/java/csci567/project/oasis/HomeActivity.java
+++ app/src/main/java/csci567/project/oasis/HomeActivity.java
@@ -13,12 +13,18 @@ import com.cloudant.client.api.ClientBuilder;
 import com.cloudant.client.api.CloudantClient;
 import com.cloudant.client.api.Database;
 import com.cloudant.client.org.lightcouch.NoDocumentException;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
+import com.ibm.mobilefirstplatform.clientsdk.android.security.googleauthentication.GoogleAuthenticationManager;
+
+import java.net.MalformedURLException;
 
 public class HomeActivity extends AppCompatActivity {
 
 	private TextView barcodeInfo;
     private Button scanButton;
     private Database db = CloudantSingleton.getInstance().getClient().database("waterfountains",false);
+    private static final int SCAN_REQUEST_CODE = 8;
+    private static final int AUTH_REQUEST_CODE = 16;
 
     private class AsyncTaskRunner extends AsyncTask<String,String, String>{
         private Exception exceptionToBeThrown;
@@ -59,11 +65,23 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 		scanButton = (Button) findViewById(R.id.btn_scan);
         barcodeInfo = (TextView) findViewById(R.id.tv_debug_qrresult);
+
+        try {
+            BMSClient.getInstance().initialize(getApplicationContext(),
+                    "http://csci567oasis.mybluemix.net",
+                    "8b14e585-8a43-41b0-ba37-4746786bbf78");
+        } catch (MalformedURLException e) {
+            Toast.makeText(HomeActivity.this, "Authentication server error", Toast.LENGTH_SHORT).show();
+        }
+
+        GoogleAuthenticationManager.getInstance().register(this);
+
+
         scanButton.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
                         Intent scanner = new Intent(HomeActivity.this, QRScanActivity.class);
-                        startActivityForResult(scanner,0);
+                        startActivityForResult(scanner,SCAN_REQUEST_CODE);
                     }
                 }
         );
@@ -85,11 +103,20 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode == RESULT_OK){
-            Bundle b = data.getExtras();
-            String s = b.getString("info");
-            AsyncTaskRunner runner = new AsyncTaskRunner();
-            runner.execute(s);
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case SCAN_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    Bundle b = data.getExtras();
+                    String s = b.getString("info");
+                    AsyncTaskRunner runner = new AsyncTaskRunner();
+                    runner.execute(s);
+                }
+                break;
+            case AUTH_REQUEST_CODE:
+                GoogleAuthenticationManager.getInstance()
+                        .onActivityResultCalled(requestCode, resultCode, data);
+                break;
         }
     }
 }
