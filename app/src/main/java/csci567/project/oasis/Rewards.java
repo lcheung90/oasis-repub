e33989b40c527c671646dc8b8143b7c98219b334
Lucas Nager <lnager@mail.csuchico.edu>
2016-05-04 13:19:08 -0700

    example rewards
    added real simple local rewards.

--- app/src/main/java/csci567/project/oasis/Rewards.java
+++ app/src/main/java/csci567/project/oasis/Rewards.java
@@ -2,13 +2,17 @@ package csci567.project.oasis;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * Created by 6661 on 5/3/2016.
- */
+
+
+
+
+
 public class Rewards extends AppCompatActivity {
 
-    public ImageView reward_konas;
+    public ImageView reward_konas, reward_kinders;
+    public TextView konas_reward, kinders_reward;
 
 
     @Override
@@ -17,6 +21,11 @@ public class Rewards extends AppCompatActivity {
         setContentView(R.layout.rewards);
 
         reward_konas = (ImageView) findViewById(R.id.reward1);
+        konas_reward = (TextView) findViewById(R.id.konas_reward);
+
+        reward_kinders = (ImageView) findViewById(R.id.reward2);
+        kinders_reward = (TextView) findViewById(R.id.kinders_reward);
+
     }
 
 
