f00ac7e0a87a6aab5502b5e935bdd84e244ac16e
Lucas Nager <lnager@mail.csuchico.edu>
2016-05-03 17:09:32 -0700

    ui additions 2
    more of my additions

--- app/src/main/java/csci567/project/oasis/HomeActivity.java
+++ app/src/main/java/csci567/project/oasis/HomeActivity.java
@@ -10,6 +10,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -222,5 +224,32 @@ public class HomeActivity extends AppCompatActivity implements ResponseListener 
                 }
             }
         }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.redeem_rewards) {
+            Log.d(TAG, "Rewards Selected");
+            //Intent redeem = new Intent(HomeActivity.this, Rewards.class);
+            //startActivity(redeem);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/csci567/project/oasis/Rewards.java
@@ -0,0 +1,10 @@
+package csci567.project.oasis;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 6661 on 5/3/2016.
+ */
+public class Rewards extends AppCompatActivity {
+
+
+}
