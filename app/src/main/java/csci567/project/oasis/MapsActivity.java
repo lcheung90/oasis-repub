78fbd108a680030ad491d60af8d74218bc5605d7
Anna Paula Maule <annamaule@usp.br>
2016-05-13 19:37:18 -0700

    direction to he water fountain retrieved from database

--- app/src/main/java/csci567/project/oasis/MapsActivity.java
+++ app/src/main/java/csci567/project/oasis/MapsActivity.java
@@ -127,6 +127,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
+
         Marker Whitney = googleMap.addMarker(new MarkerOptions().position(WHITNEY).alpha(0).title("Whitney Hall"));
         Marker Albert = googleMap.addMarker(new MarkerOptions().position(ALBERT).alpha(0).title("Albert"));
 
